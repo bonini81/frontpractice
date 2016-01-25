183e98e090488b0daba192f9c3ddf4e59127b2a0
Andrés Domínguez <pugle81@gmail.com>
2016-01-24 21:19:39 -0500

    Created goals.php and added some content

---
+++ goals.php
@@ -0,0 +1,49 @@
+
+<?php
+include("includes/header.php"); 
+?>
+
+
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main jumbotron">
+        <h2>GOALS 2016</h2>
+       
+              <h3>WORK</h3>
+       <ul>
+    
+<li>Start creating and Agency and Products, Freelance is not that profitable</li>
+<li>Consolidate Cashflow or Get a Job man.</li>
+<li>Go to Guayaquil to check out growing start up scene</li>
+       </ul>
+
+
+
+ <h3>LIFE</h3>
+
+ <h3>TRAVEL</h3>
+
+  <h3>I+D+I</h3>
+  <ul>
+  <li><strong>Front End 101 - Fase 1</strong></li> 
+  <li>Bootstrap</li>
+  <li>LESS</li>
+  <li>Grunt</li>
+  <li>GIT and GitHub</li>
+    <li><strong>Front End 101 - Fase 2</strong></li> 
+      <li>Angular JS</li>
+      
+
+  </ul>
+
+   <h3>ESPIRITUAL</h3>
+
+
+      </div>
+
+
+
+
+  <?php
+
+include("includes/footer.php");
+
+  ?>
