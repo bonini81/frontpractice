a1eb00350dc5db0c6bc939c52c5eb15c7fba97e4
Andrés Domínguez <pugle81@gmail.com>
2016-03-29 01:17:19 -0500

    Well updates on previous commit

--- investiga.php
+++ investiga.php
@@ -6,20 +6,23 @@ include("includes/header.php");
  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">I+D - TrackBoard</h1>
 
-
+<ol class="breadcrumb">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Tracking</a></li>
+  <li class="active">I+D</li>
+</ol>
 
          
 
-          <h2 class="sub-header">Section title</h2>
-<br/>
-
+         
 
           <button type="button" class="btn btn-default" aria-label="Left Align">
 
  Add Task <span class="glyphicon glyphicon-plus-sign"></span>
 </button>
 
-<br/>
+<br/><br/>
+
 
           <div class="table-responsive">
             <table class="table table-striped">
@@ -29,6 +32,7 @@ include("includes/header.php");
                   <th>Fecha</th>
                   <th>Categoria</th>
                   <th>To do</th>
+                     <th>Priority</th>
                   <th>So?</th>
                 </tr>
               </thead>
@@ -38,120 +42,57 @@ include("includes/header.php");
                   <td>29/03/2016</td>
                   <td>Front</td>
                   <td>Add buttons icons with Bootstrap, get Familiar.</td>
+                    <td>Normal</td>
                   <td>Not Yet</td>
                 </tr>
                 <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
+                  <td>2</td>
+                  <td>29/03/2016</td>
+                  <td>Version Control</td>
+                  <td>Learn last shit of Git, before closing Plazti account for a while.</td>
+                      <td>High</td>
+                  <td>Not Yet</td>
                 </tr>
                 <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
+                  <td>3</td>
+                  <td>29/03/2016</td>
+                    <td>BackEnd</td>
+                  <td>Think about Classes and objetcts to use with PHP</td>
+                      <td>Normal</td>
+                  <td>Not Yet</td>
                 </tr>
                 <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
+                  <td>4</td>
+                  <td>29/03/2016</td>
+                  <td>Version Control</td>
+                  <td>Can git save databases, if so how? and Checkout WP video in PLatzi</td>
+                      <td>High</td>
+
+                  <td>Not Yet</td>
                 </tr>
                 <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
+                  <td>5</td>
+                  <td>29/03/2016</td>
+                  <td>BackEnd</td>
+                  <td>Implement DB in here and some CRUD shit</td>
+                      <td>Normal</td>
+                  <td>Not Yet</td>
                 </tr>
                 <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
+                  <td>6</td>
+                  <td>29/03/2016</td>
+                  <td>UI</td>
+                  <td>Check Responsive UI in this project, menus and tables not responsive</td>
+                  
+                      <td>High</td>
+                  <td>Not Yet</td>
                 </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
-                </tr>
+           
+               
               </tbody>
             </table>
           </div>
-        </div>
-      </div>
-    </div>
-
+     </div>
 
 
 
